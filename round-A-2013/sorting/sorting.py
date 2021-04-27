56292d860f5adb3ff022b53d00f0a40050790167
bhanuprasad1999 <bhanuprasad1999@github.com>
2021-04-27 19:16:02 +0530

    commit

---
+++ round-A-2013/sorting/sorting.py
@@ -0,0 +1,30 @@
+t = int(input())
+even = []
+evenindex = []
+odd = []
+oddindex = []
+new_array = []z
+for _ in range(t):
+    n = int(input())
+    a = [int(i) for i in input().split(' ')]
+    for i in range(len(a)):
+        if a[i]%2 == 0:
+            even.append(a[i])
+            evenindex.append(i)
+        else:
+            odd.append(a[i])
+            oddindex.append(i)
+    even.sort()
+    odd.sort()
+    even = even[::-1]
+
+    j = 0
+    k = 0
+    for i in range(len(a)):
+        if a[i]%2 == 0:
+            a[i] = even[j]
+            j += 1
+        else:
+            a[i] = odd[k]
+            k += 1
+    print(f"Case #{_+1}: {a}")
